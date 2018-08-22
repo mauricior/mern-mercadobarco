c9c013836ea4b8f5b20f12645074441b9a565e1a
Mauricio Reis <mauriciofsr@outlook.com.br>
2018-08-22 14:36:38 -0300

    Updated the NavBar

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -24,13 +24,23 @@ class AppNavbar extends Component {
       <div>
         <Navbar color="light" light expand="sm" className="mb-5">
           <Container>
-            <NavbarBrand href="/">MercadoBarco</NavbarBrand>
+            <NavbarBrand href="/">Mercado Barco</NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
                   <NavLink href="https://github.com/MauricioR">
                     Github
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/signup">
+                    Signup
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/sigin">
+                    Signin
                   </NavLink>
                 </NavItem>
               </Nav>
