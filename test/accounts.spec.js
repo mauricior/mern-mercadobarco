3a77ae6d49a22ff5f8fe300b7d9b8b7ac6dd5b7d
MauricioR <mauriciofsr@outlook.com.br>
2018-08-21 23:38:28 -0300

    Add the test folder and the unit tests, start to dev using TDD

---
+++ test/accounts.spec.js
@@ -0,0 +1,21 @@
+const should = require("should");
+const request = require("request");
+const chai = require("chai");
+const expect = chai.expect;
+const accounts = require('../routes/api/accounts');
+const urlBase = "https://localhost:5000";
+
+describe('Testing API accounts route', () => {
+  it('should return the users by username', () => {
+
+  });
+
+  it('should return the users by email', () => {
+
+  });
+
+  it('should register a new user', () => {
+
+  });
+
+});
---
+++ test/adsboats.spec.js
@@ -0,0 +1,25 @@
+const should = require("should");
+const request = require("request");
+const chai = require("chai");
+const expect = chai.expect;
+const accounts = require('../routes/api/accounts');
+const urlBase = "https://localhost:5000";
+
+describe('Testing adsboats api route', () => {
+
+  it('should return all the adsboats', () => {
+
+  });
+
+  it('should return the adsboats by the type', () => {
+
+  });
+
+  it('should add a new adboat', () => {
+
+  });
+
+  it('should delete a adboat', () => {
+
+  });
+});
