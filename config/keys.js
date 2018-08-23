201159212854dc578c8cf62c2dfe9e7e0958f987
MauricioR <mauriciofsr@outlook.com.br>
2018-08-23 20:05:15 -0300

    Changed the mongoURI to work in my localhost workstation

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   // mlab URI, use on a work environment without a proxy
-  //mongoURI: 'mongodb://mern-admin:FSRSolutions1@ds227332.mlab.com:27332/mern-mercadobarco'
+  mongoURI: 'mongodb://mern-admin:FSRSolutions1@ds227332.mlab.com:27332/mern-mercadobarco'
   // Localhost mongoURI
-  mongoURI: 'mongodb://localhost:27017/mern-mercadobarco'
+  // mongoURI: 'mongodb://localhost:27017/mern-mercadobarco'
 }
