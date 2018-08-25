5518da01610ff1cda8b5cd2be33603701c33587b
MauricioR <mauriciofsr@outlook.com.br>
2018-08-24 21:10:05 -0300

    Updated FindAd component and added to Home component

--- client/src/components/FindAd.js
+++ client/src/components/FindAd.js
@@ -1,12 +1,69 @@
 import React, { Component } from 'react';
-
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
 
 class FindAd extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      activeTab: '1'
+    };
+  }
+
+  toggle(tab) {
+    if(this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
   render() {
     return (
       <div>
-        <h1>FindAd</h1>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '1'})}
+              onClick={() => { this.toggle('1'); }}
+              >
+              Lanchas
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '2'})}
+              onClick={() => { this.toggle('2'); }}
+              >
+              Veleiros
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '3'})}
+              onClick={() => { this.toggle('3'); }}
+              >
+              Jet Skis
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12">
+                <Card body>
+                  <CardTitle>Lanchas</CardTitle>
+                  <CardText>Filter</CardText>
+                  <Button>Search</Button>
+                </Card>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
       </div>
     );
   }
 }
+
+export default FindAd;
