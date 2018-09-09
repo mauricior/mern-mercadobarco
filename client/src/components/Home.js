cb86d58abb38d2c4e3bd97d1b82ea85a9c9f25cd
MauricioR <mauriciofsr@outlook.com.br>
2018-09-09 15:10:32 -0300

    Modified Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import FindAd from './FindAd';
+import Ads from './Ads';
 
 class Home extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      sellItems: [{id: 1, itemName: "Lanchas", filter: {type: '', manufacturer: '', minValue: '', maxValue: '', minFoot: '', maxFoot: '', location: ''}},
+                  {id: 2, itemName: "Veleiros", filter: {type: '', manufacturer: '', minValue: '', maxValue: '', minFoot: '', maxFoot: '', location: ''}},
+                  {id: 3, itemName: "Jet Skis", filter: {type: '', manufacturer: '', minValue: '', maxValue: '', minFoot: '', maxFoot: '', location: ''}}]
+    }
+  }
   render() {
     return (
       <div>
         <h1>Home</h1>
-        <FindAd />
+        <FindAd items={this.state.sellItems} />
+        <Ads />
       </div>
     );
   }
