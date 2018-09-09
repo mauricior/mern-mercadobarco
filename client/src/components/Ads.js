ddece70a98c5e8822e0c34fc7fbb8ec939a6c577
MauricioR <mauriciofsr@outlook.com.br>
2018-09-09 15:09:25 -0300

    Modified Ads component

---
+++ client/src/components/Ads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Ads extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Recently Ads</h1>
+      </div>
+    );
+  }
+}
+
+export default Ads;
